fc9817c1e557b39655fbcf292b6eb590515dc259
DiagonalMatrix <srujan.crm@gmail.com>
2018-10-23 20:41:27 +0100

    ComplexTypes Object for dataset.

--- src/main/scala/dataset/ComplexTypes.scala
+++ src/main/scala/dataset/ComplexTypes.scala
@@ -1,9 +1,16 @@
 package dataset
 
 object ComplexTypes {
+
+  case class Point(x: Double, y: Double)
+  case class Segment(from: Point, to: Point)
+  case class Line(name: String, points: Array[Point])
+  case class NamedPoints(name: String, points: Map[String, Point])
+  case class NameAndMayBePoint(name: String, point: Option[Point])
+
   def main(args: Array[String]): Unit = {
 
-    case class Point(x: Double, y: Double)
+    
 
 
   }
